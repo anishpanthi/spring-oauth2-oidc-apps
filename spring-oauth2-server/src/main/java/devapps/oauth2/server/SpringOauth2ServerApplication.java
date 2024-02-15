6ea67f6656e5dbc502a751a9e10520c74d5d1919
Anish <anish.panthi41@gmail.com>
2024-02-14 20:06:42 -0600

    Initial Commit

---
+++ spring-oauth2-server/src/main/java/devapps/oauth2/server/SpringOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package devapps.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ServerApplication.class, args);
+	}
+
+}
