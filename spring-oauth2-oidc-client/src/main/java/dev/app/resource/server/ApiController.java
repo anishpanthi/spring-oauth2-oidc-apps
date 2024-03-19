e510d5909980bad278f414f6b364f61e08db0652
Anish <anish.panthi41@gmail.com>
2024-03-18 19:13:44 -0500

    Updated controller

--- spring-oauth2-oidc-client/src/main/java/dev/app/resource/server/ApiController.java
+++ spring-oauth2-oidc-client/src/main/java/dev/app/resource/server/ApiController.java
@@ -4,6 +4,10 @@ import java.security.Principal;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody
 public class ApiController {
 
+  private final OAuth2AuthorizedClientService authorizedClientService;
+
+  public ApiController(OAuth2AuthorizedClientService authorizedClientService) {
+    this.authorizedClientService = authorizedClientService;
+  }
+
   @GetMapping("/api/greet")
   public Map<String, String> greet(Principal principal) {
+
+    OAuth2AccessToken accessToken =
+        authorizedClientService
+            .loadAuthorizedClient("azure-b2c", principal.getName())
+            .getAccessToken();
+    OAuth2RefreshToken refreshToken =
+        authorizedClientService
+            .loadAuthorizedClient("azure-b2c", principal.getName())
+            .getRefreshToken();
+    log.info("Access Token: {}", accessToken.getTokenValue());
+    log.info(
+        "Refresh Token: {}",
+        refreshToken == null ? "null" : refreshToken.getTokenValue());
     return Map.of(
         "message",
         "Hello " + principal.getName() + "! Welcome to Spring's OAuth2.0 & OpenID Connect demo");
